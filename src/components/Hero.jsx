0470a4061b55411e27ee1dd6d601dd848dae6e27
heysourin <sourin07@gmail.com>
2022-12-09 23:43:41 +0530

    Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,81 @@
 import React from 'react'
-import avatar from "../assets/owner.jpg"
+import avatar from '../assets/owner.jpg'
+import { SocialIcon } from 'react-social-icons'
 
 const Hero = () => {
   return (
     <div
-      className="bg-[url('https://pixabay.com/get/g0f745cf3ea76c4de1fd68cff7c3eb516c9f6c995ab684b9e0b0c3c7aae4cef6177e2bf87cc7db5fe2108f6942aff8594a73e224e3c1b952f7fc21f390646502b_1920.jpg')]
-        bg-no-repeat bg-cover">
-
+      className="bg-[url('https://images.unsplash.com/photo-1563089145-599997674d42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')]
+        bg-no-repeat bg-cover"
+    >
       <div className="flex flex-col justify-center items-center mx-auto py-10">
-
-        <h1 className="text-white text-5xl font-bold text-center">A.I Arts <br />
-        <span>NFTs </span>Collection
+        <h1 className="text-white text-5xl font-bold text-center">
+          A.I Arts <br />
+          <span>NFTs </span>Collection
         </h1>
 
-        <p className='text-white font-semibold text-sm'>Mint & COllect The Hottest NFTs Around</p>
+        <p className="text-white font-semibold text-sm">
+          Mint & COllect The Hottest NFTs Around
+        </p>
 
-        <button type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 my-3">Mint NFT</button>
+        <button
+          type="button"
+          class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 my-3"
+        >
+          Mint NFT
+        </button>
 
-
-        <a href="https://daltonic.github.io" className='flex justiy-center items-center space-x-2 bg-[#000000ad] rounded-full my-4 pr-3'>
-            <img className='w-11 h-11 object-contain rounded-full' src={avatar} alt="" />
-            <div className='flex flex-col text-white font-semibold text-sm'>
-                <span>0x5465..1a3</span>
-                <span className='text-[#e32970]'>Ryle</span>
-            </div>
+        <a
+          href="https://github.com/heysourin"
+          className="flex justiy-center items-center space-x-2 bg-[#000000ad] rounded-full my-4 pr-3"
+        >
+          <img
+            className="w-11 h-11 object-contain rounded-full"
+            src={avatar}
+            alt=""
+          />
+          <div className="flex flex-col text-white font-semibold text-sm">
+            <span>0x5465..1a3</span>
+            <span className="text-[#e32970]">Max</span>
+          </div>
         </a>
 
-        
+
+{/* Social Medias  */}
+        <ul className="flex flex-row justify-center space-x-0.5 items-center">
+          {/* <div className="flex items-center space-x-6 lg:space-x-5"> */}
+          <SocialIcon
+            className="bg-white scale-75 hover:scale-50 transition-all duration-75 delay-75 rounded-full"
+            url="https://github.com/heysourin"
+            target="_blank"
+          />
+          <SocialIcon
+            className="bg-white scale-75 hover:scale-50 transition-all duration-75 delay-75 rounded-full"
+            url="https://linkedin.com/in/heysourin"
+            target="_blank"
+          />
+          <SocialIcon
+            className="bg-white scale-75 hover:scale-50 transition-all duration-75 delay-75 rounded-full"
+            url="https://twitter.com/heysourin"
+            target="_blank"
+          />
+          <SocialIcon
+            className="bg-white scale-75 hover:scale-50 transition-all duration-75 delay-75 rounded-full"
+            url="https://instagram.com/hey.sourin"
+            target="_blank"
+          />
+          <SocialIcon
+            className="bg-white scale-75 hover:scale-50 transition-all duration-75 delay-75 rounded-full"
+            url="https://discord.com/channels/heysourin#9127"
+            target="_blank"
+          />
+        </ul>
+
+
+ {/* NFT Count  */}
+        <div className="shadow-xl shadow-black flex justify-center items-center w-10 h-10 rounded-full bg-white cursor-pointer p-3 m-4 text-black hover:bg-[#000] hover:text-white transaction-all duration-75 delay-100 ">
+          <span className='text-md font-bold'>99</span>
+        </div>
       </div>
     </div>
   )
@@ -34,3 +83,4 @@ const Hero = () => {
 
 export default Hero
 // src\assets\heroimage.png
+//Photo by Uzunov Rostislav from Pexels: https://www.pexels.com/photo/digital-animation-of-colorful-tape-rolls-10458835/
