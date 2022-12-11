a7106fa2df1750d58c7360f80fbad2623e259eea
heysourin <sourin07@gmail.com>
2022-12-11 12:54:48 +0530

    Contract Updated

--- contracts/main.sol
+++ contracts/main.sol
@@ -3,8 +3,130 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-contract  Token{
-    constructor() {
-        
+
+contract MyToken is ERC721Enumerable, Ownable {
+    using Strings for uint256; //Converting uin to string
+
+    string baseURI;
+    string public baseImage = ".webp";
+    string public baseExtension = ".json"; //Used for gathering informaation for each nft
+    uint public cost = 0.0001 ether; //Cost to mint each nft
+    uint public maxSupply = 99;
+    bool public paused = false;
+
+    event Sale(
+        uint id,
+        address indexed buyer,
+        uint cost,
+        string indexed tokenURI,
+        uint timestamp
+    );
+
+    struct SaleStruct {
+        uint id;
+        address buyer;
+        uint cost;
+        string imageURL;
+        uint timestamp;
+    }
+
+    SaleStruct[] public minted;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _initBaseURI
+    ) ERC721(_name, _symbol) {
+        setBaseURI(_initBaseURI);
+    }
+
+    function payToMint() public payable {
+        uint supply = totalSupply();//total available supply
+        require(!paused, "On Pause, NFT Under Maintenance");
+        require(supply <= maxSupply, "Sorry, all NFTs have been minted");
+        require(msg.value > 0 ether, "Too low");
+
+        if (msg.sender != owner()) {
+            require(msg.value >= cost, "Please top up ethers");
+        }
+
+        _safeMint(msg.sender, supply + 1);
+
+        minted.push(
+            SaleStruct(
+                supply + 1,
+                msg.sender,
+                msg.value,
+                toImage(supply + 1),
+                block.timestamp
+            )
+        );
+
+        emit Sale(supply, msg.sender, msg.value, tokenURI(supply + 1), block.timestamp);
+    }
+
+    function tokenURI(
+        uint tokenId
+    ) public view virtual override returns (string memory) {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory currentBaseURI = _baseURI();
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        tokenId.toString(),
+                        baseExtension
+                    )
+                )
+                : "";
+    }
+
+    function toImage(uint tokenId) internal view returns (string memory) {
+        string memory currentBaseURI = _baseURI();
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        tokenId.toString(),
+                        baseImage
+                    )
+                )
+                : "";
+    }
+
+    function getAllNFTs() public view returns (SaleStruct[] memory) {
+        return minted;
+    }
+
+    function getAnNFT(uint tokenId) public view returns (SaleStruct memory) {
+        return minted[tokenId - 1];
+    }
+
+    function payTo(address to, uint amount) public onlyOwner {
+        (bool success1, ) = payable(to).call{value: amount}("");
+        require(success1);
+    }
+
+    function setBaseURI(string memory _newBAseURI) public onlyOwner {
+        baseURI = _newBAseURI;
+    }
+
+    function setPause(bool _state) public onlyOwner {
+        paused = _state;
+    }
+
+    function _basaeURI()
+        internal
+        view
+        virtual
+        returns (string memory)
+    {
+        return baseURI;
     }
 }
