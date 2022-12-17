c48a2c90116e1a2b2a641b5f6db35a220231e011
heysourin <sourin07@gmail.com>
2022-12-17 20:21:48 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,34 @@
 import React from 'react'
 import ethlogo from '../assets/ethlogo.png'
 import { connectWallet } from '../contract'
+import { truncate, useGlobalState } from '../store'
 
 const Header = () => {
-
+  const [connectedAccount] = useGlobalState('connectedAccount')
   return (
-    <nav className='w-full flex md:justify-center justify-between items-center py-4 mx-auto bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-10'>
-        <div className='flex flex-row justify-start items-center md:flex-[0.5] flex-initial'>
-            <img className='w-15 h-20 cursor-pointer' src={ethlogo} alt="" />
-            <span className='text-white text-2xl ml-2 '>Infinity ∞</span>
-        </div>
-        
-        <ul className='md:flex-[0.5] text-white md:flex list-none flex-row justify-between items-center flex-initial px-20'>
-          <li className='mx-4 cursor-pointer '>Explore </li>
-          <li className='mx-4 cursor-pointer '>Features </li>
-          <li className='mx-4 cursor-pointer '>Community </li>
-        </ul>
+    <nav className="w-full flex md:justify-center justify-between items-center py-4 mx-auto bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-10">
+      <div className="flex flex-row justify-start items-center md:flex-[0.5] flex-initial">
+        <img className="w-15 h-20 cursor-pointer" src={ethlogo} alt="" />
+        <span className="text-white text-2xl ml-2 ">Infinity ∞</span>
+      </div>
 
-        <button className='bg-purple-600 hover:bg-purple-800 text-white font-bold py-2 px-4 rounded-full shadow-lg shadow-gray-100/50'
-        onClick={connectWallet}> Connect Wallet </button>
+      <ul className="md:flex-[0.5] text-white md:flex list-none flex-row justify-between items-center flex-initial px-20">
+        <li className="mx-4 cursor-pointer ">Explore </li>
+        <li className="mx-4 cursor-pointer ">Features </li>
+        <li className="mx-4 cursor-pointer ">Community </li>
+      </ul>
+      {connectedAccount ? (
+        <button className="bg-purple-600 hover:bg-purple-800 text-white font-bold py-2 px-4 rounded-full shadow-lg shadow-gray-100/50">
+          {truncate(connectedAccount,4,4,11)}
+        </button>
+      ) : (
+        <button
+          className="bg-purple-600 hover:bg-purple-800 text-white font-bold py-2 px-4 rounded-full shadow-lg shadow-gray-100/50"
+          onClick={connectWallet}
+        >
+          Connect Wallet
+        </button>
+      )}
     </nav>
   )
 }
